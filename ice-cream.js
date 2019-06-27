38137547854b316fa8dd08589fa6e43be4b8a51a
allisonnnnnn <allisonchen1130@gmail.com>
2019-06-27 19:48:44 +0000

    changed

---
+++ ice-cream.js
@@ -0,0 +1,15 @@
+const iceCreamFlavours = [
+  "chocolate",
+  "vanilla",
+  "cookies and cream",
+  "rocky road",
+  "strawberry"
+];
+
+iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours[5]);
+
+console.log("Bob" || " Marley");
+console.log("Jeremy" > "Khurram");
+console.log("Don\nBurks");
